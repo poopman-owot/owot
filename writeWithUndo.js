4608d7f7f5706b029552a53dcd1be0e487650177
KyleCypher-kv <kcypher@saec-kv.com>
2019-12-05 22:08:26 -0600

    fixed undo all

--- writeWithUndo.js
+++ writeWithUndo.js
@@ -1,50 +1,50 @@
-function writeWithUndo() 
-    { 
-        var localEdits = [];
+function writeWithUndo() {
+    alert("Ctrl+Z: Undo\nCtrl+Alt+Z: Undo All")
+    var localEdits = [];
 
-        function checkForUndo(e) {
-            var ctrl = e.ctrlKey;
-            var alt = e.altKey;
-            var zKey = e.key.toLowerCase() == "z";
-            if (ctrl && zKey && !alt) {
-                undo();
-            }
-            if (ctrl && zKey && alt) {
-                undoAll();
-            }
+    function checkForUndo(e) {
+        var ctrl = e.ctrlKey;
+        var alt = e.altKey;
+        var zKey = e.key.toLowerCase() == "z";
+        if (ctrl && zKey && !alt) {
+            undo();
         }
-        function undo() {
-            if (!localEdits) { return };
-            var lastEdit = localEdits[localEdits.length - 1];
-            if (!lastEdit) { return };
-            var tileX = lastEdit[1].tileX;
-            var tileY = lastEdit[1].tileY;
-            var charX = lastEdit[1].charX;
-            var charY = lastEdit[1].charY;
-            var char = lastEdit[0].char;
-            var color = lastEdit[0].color;
-
-            renderCursor([tileX, tileY, charX, charY]);
-            writeCharTo(char, color, tileX, tileY, charX, charY);
-            localEdits.pop()
-        };
-
-        function undoAll() {
-            var undoInterval = setInterval(doUndo, 5);
-            function doUndo() {
-                if (localEdits) { undo(); }
-                else { stopUndoInterval(); }
-            };
-            function stopUndoInterval() {
-                clearInterval(undoInterval);
-            }
-        };
-
-        document.addEventListener("keydown", function (e) { checkForUndo(e); });
-
-        w.on("writeBefore", function (data) {
-            var charInfo = getCharInfo(data.tileX, data.tileY, data.charX, data.charY);
-            localEdits.push([charInfo, data]);
-        })
+        if (ctrl && zKey && alt) {
+            undoAll();
+        }
     }
+    function undo() {
+        if (!localEdits) { return };
+        var lastEdit = localEdits[localEdits.length - 1];
+        if (!lastEdit) { return };
+        var tileX = lastEdit[1].tileX;
+        var tileY = lastEdit[1].tileY;
+        var charX = lastEdit[1].charX;
+        var charY = lastEdit[1].charY;
+        var char = lastEdit[0].char;
+        var color = lastEdit[0].color;
+
+        renderCursor([tileX, tileY, charX, charY]);
+        writeCharTo(char, color, tileX, tileY, charX, charY);
+        localEdits.pop()
+    };
+
+    function undoAll() {
+        var undoInterval = setInterval(doUndo, 5);
+        function doUndo() {
+            if (localEdits.length > 0) { undo(); }
+            else { stopUndoInterval(); }
+        };
+        function stopUndoInterval() {
+            clearInterval(undoInterval);
+        }
+    };
+
+    document.addEventListener("keydown", function (e) { checkForUndo(e); });
+
+    w.on("writeBefore", function (data) {
+        var charInfo = getCharInfo(data.tileX, data.tileY, data.charX, data.charY);
+        localEdits.push([charInfo, data]);
+    })
+}
 writeWithUndo();
