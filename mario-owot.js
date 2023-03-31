e6085715c8f20004ecc0cf714b935cf170093515
poopman <kylecypher@gmail.com>
2023-03-31 13:02:49 -0500

    Update mario-owot.js

--- mario-owot.js
+++ mario-owot.js
@@ -1,4 +1,4 @@
-const marioScriptVersion = "v.1.27";
+const marioScriptVersion = "v.1.28";
 function loadScript(url, callback) {
   var script = document.createElement('script');
   script.type = 'text/javascript';
