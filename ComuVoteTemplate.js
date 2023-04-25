c5f845f5cc9fbb6782a9d8638abb25026a2151cd
poopman <kylecypher@gmail.com>
2023-04-25 07:48:25 -0500

    Update ComuVoteTemplate.js

--- ComuVoteTemplate.js
+++ ComuVoteTemplate.js
@@ -1,76 +1,112 @@
 
-//settings
-
-const comu_event = "example_event"; //EXAMPLE LINKS:     comu:example_event & comu:{"example_event":true}
-const handleJSON = true; //Handle JSON related data
-const handleNonJSON = true; //Handle non-JSON data
-const allowAnon = true; //Allow anon comu clicks
-const preventSpam = true; //Prevent spam by comparing to an array of IDs
+//SETTINGS
+var comu_settings = {
+  event_Name: "example_event", //EXAMPLE LINKS:     comu:{"example_event":yes}
+  handle_JSON: true, //Handle JSON related data
+  handle_Non_JSON: true, //Handle non-JSON data
+  allow_Anon: true, //Allow anon comu clicks
+  prevent_Spam: false, //Prevent spam by comparing to an array of IDs
+  clear_Console: true, //Clear the console before logging the votes
+  log_Console: true, //Show the votes in the console
+}
+//----------------------------------------------------------------------
+//Inital Variables
+let preventSpam_list = []; //Stores IDs to help reduce chance of spam
+let votes = {
+  yes: 0,
+  no: 0
+}; //This is the votes tally
+w.broadcastReceive(); //turn on ability to recieve messages
 //----------------------------------------------------------------------
 
-let preventSpam_list = [];
-let votes = 0;
-w.broadcastReceive(); //turn on ability to recieve messages
-
-
-
-function handle_comu_event(data) {
-  if (data.id == undefined && !allowAnon) {
+//Main Function
+function handle_comu_event(e) {
+  if (e.id == undefined && comu_settings.allow_Anon == false) {
     //if an anon is illegally voting, call the police.
     return false;
   }
-
-  if (preventSpam) {
-    if (addToPreventSpamList(data.id, data.sender)) {
+  if (comu_settings.prevent_Spam) {
+    if (addToPreventSpamList(e.id, e.sender)) {
       //if a new entry was just added, count the vote.
-      votes++;
+      handle_comu_data(e);
     } else {
       return false;
     }
   } else {
-    //count the vote, ignoring spam.
-    votes++;
+    //count the vote, ignoring spam filter.
+    handle_comu_data(e);
   }
-  console.log(comu_event, votes);
 }
 
+function handle_comu_data(e) {
+  //Check if the data is in JSON format
+  const data = e.data;
+  if (isJsonString(data)) {
+    //capture json data
+    const jsonData = JSON.parse(data);
+    if (jsonData[comu_settings.event_Name]) {
+			//extract vote data
+      const vote = jsonData[comu_settings.event_Name];
+      //add to vote tally
+      votes[vote]++;
+    }
+  } else {
+    //handle non-JSON
+		//extract vote data
+    const vote = data.split('$')[1];
+		//add to vote tally
+    votes[vote]++;
+  }
 
+
+  //logging
+  if (comu_settings.log_Console) {
+    if (comu_settings.clear_Console) {
+      console.clear();
+    }
+    console.log(votes);
+  }
+}
+//----------------------------------------------------------------------
+//Main Event Listener
 //this is the logic for reciving the message but doesnt need to modified from here. instead modify the handle_comu_event function.
-w.on('cmd', function(comu) {
-  if (handleJSON) {
-    const data = comu.data;
+w.on('cmd', function(e) {
+  if (comu_settings.handle_JSON) {
+    const data = e.data;
     if (isJsonString(data)) {
       //capture json data
       const jsonData = JSON.parse(data);
-      if (jsonData[comu_event]) {
-        handle_comu_event(comu);
+      if (jsonData[comu_settings.event_Name]) {
+        handle_comu_event(e);
       }
     }
   }
-  if (handleNonJSON) {
-    if (!isJsonString(comu.data)) {
-      if (comu.data == comu_event) {
-        handle_comu_event(comu);
+  if (comu_settings.handle_Non_JSON) {
+    if (!isJsonString(e.data)) {
+      if (e.data.includes(comu_settings.event_Name)) {
+        handle_comu_event(e);
       }
     }
   }
 })
+//----------------------------------------------------------------------
+//HELPER FUNCTIONS
 //this function is used to determine if the string is valid without throwin errors in cases its not.
-function isJsonString(str) {
+function isJsonString(a) {
   try {
-    JSON.parse(str);
+    JSON.parse(a);
   } catch (e) {
     return false;
   }
   return true;
 }
 //this is the logic to determine if prevent spam list contains the same ID.
-const addToPreventSpamList = (string1, string2) => {
-  if (!preventSpam_list.includes(string1) && !preventSpam_list.includes(string2)) {
-    if (string1 !== undefined) {
-      preventSpam_list.push(string1);
+const addToPreventSpamList = (a, b) => {
+  if (!preventSpam_list.includes(a) && !preventSpam_list.includes(b)) {
+    if (a !== undefined) {
+      preventSpam_list.push(a);
     }
-    preventSpam_list.push(string2);
+    preventSpam_list.push(b);
     return true
   } else {
     return false
